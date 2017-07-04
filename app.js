569ae24bc8ae5d71cc7c3df8b0b6e8179a05abbe
Eduardo Caetano <ecaetanocorrea@gmail.com>
2017-07-03 22:24:08 -0300

    conectando a o banco online

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ var TwitterPackage = require('twitter');
 
 var mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/twitterDBS');
+mongoose.connect('mongodb://user2:123456@ds139942.mlab.com:39942/kopteste');
+//mongoose.connect('mongodb://localhost/twitterDBS');
+
 var a = 1;
 var secret = {
   consumer_key: 'qKjOCsSCHNIKgwpIq8QORrlh9',
@@ -10,28 +12,33 @@ var secret = {
   access_token_key: '290752188-EmQCY3cgrILIbALQBsJ0bDClWUXWQuAVmskfKukN',
   access_token_secret: 'AJ90q7To2OrfAw20R7U5ynvZ8seJCaSFbBZ9ywwkLjzPk'
 }
-var Post = mongoose.model('Post', { text: String });
+
+var Post = mongoose.model('Post', {
+  owner: String, 
+  id: String,
+  user: String,
+  text: String,
+  data: Date
+
+});
 
 var Twitter = new TwitterPackage(secret);
 
-Twitter.stream('statuses/filter', {track: '@realDonaldTrump'}, function(stream) {
+Twitter.stream('statuses/filter', {track: '@whindersson'}, function(stream) {
   stream.on('data', function(tweet) {
-    console.log(tweet.text);
-    
-  
 
-// modelo 
+    console.log("Id:"+tweet.id+"Usuario: "+tweet.user.name+"\nTweet: "+tweet.text);
+    //console.log("Usuario: "+tweet.user.name+"\nNome Real: "+screen_name+"\nHashtags: "+tweet.entities.hashtags+"\nQuem foi mencionado:"+entities.user_mentions+"\nTweet: "+tweet.text);
+    //console.log(tweet);
+    //console.log(tweet.entities.hashtags[0,1]);
 
-// segundos = segundos - 1;
-// var Post = mongoose.model('Post', { text: tweet.text });
+var post = new Post({ owner: "user2", id: tweet.id, user: tweet.user.name, text: tweet.text, data: tweet.timestamp_ms    });
 
-var post = new Post({ text: tweet.text });
-a = a + 1;
 post.save(function (err) {
           if (err) {
             console.log(err);
           } else {
-            console.log('--------------------------------------------- Salvo no banco tweet '+a+'---------------------------------');
+            console.log('--------------------------------------------- Salvo no banco tweet ---------------------------------');
           }
 });
 
